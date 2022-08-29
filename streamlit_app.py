283dec3d396e20ce667d8b386f3f328e471b68f2
kiranrode07 <39567496+kiranrode07@users.noreply.github.com>
2022-08-29 10:41:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title ('My parents new healthy dinner')
+
+streamlit.header ('Breakfast Menu')
+streamlit.title ('Omega 3 & Blueberry oat meal')
+streamlit.title ('Kale, spinnach and rocket smoothie')
+streamlit.title ('Hard boiled free-range eggs')
